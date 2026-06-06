7503a1d76db3f0c60401fe839a89aac56f8b48e6
goldenxgpacking-rgb <goldenxgpacking@mail.com>
2026-06-06 22:49:22 +0630

    test: 降级geolocator到^10.0.0,测试兼容性(Build #25)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:geolocator/geolocator.dart'; // TODO: fix geolocator
-// import 'package:permission_handler/permission_handler.dart'; // TODO: fix permission_handler
+import 'package:geolocator/geolocator.dart';
+// import 'package:permission_handler/permission_handler.dart'; // Already tested separately
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -28,13 +28,35 @@ class _HomeScreenState extends State<HomeScreen> {
       _isLoadingLocation = true;
     });
 
-    // TODO: Re-enable geolocator after fixing CI
-    // Mock location for now
-    await Future.delayed(const Duration(seconds: 1));
-    setState(() {
-      _currentAddress = 'Mock Location (16.8661, 96.1951)';
-      _isLoadingLocation = false;
-    });
+    // Request permission
+    var status = await Permission.location.status;
+    if (!status.isGranted) {
+      status = await Permission.location.request();
+    }
+
+    if (status.isGranted) {
+      try {
+        // Geolocator v10 API (different from v11)
+        Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high,
+        );
+        setState(() {
+          _currentAddress =
+              'Lat: ${position.latitude.toStringAsFixed(4)}, Lng: ${position.longitude.toStringAsFixed(4)}';
+          _isLoadingLocation = false;
+        });
+      } catch (e) {
+        setState(() {
+          _currentAddress = 'Unable to get location';
+          _isLoadingLocation = false;
+        });
+      }
+    } else {
+      setState(() {
+        _currentAddress = 'Location permission denied';
+        _isLoadingLocation = false;
+      });
+    }
   }
 
   void _toggleOnlineStatus() {
