c27e2dea06df1862d909d53eeaec906a273fdc6f
goldenxgpacking-rgb <goldenxgpacking@mail.com>
2026-06-08 16:42:06 +0630

    fix: add missing HomeScreen import in trip_completed_screen

--- lib/screens/trip_completed_screen.dart
+++ lib/screens/trip_completed_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'earnings_screen.dart';
+import 'home_screen.dart';
 
 class TripCompletedScreen extends StatelessWidget {
   final String passengerName;
